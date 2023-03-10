d8a8682470e8d4a03d633bb20e33a9c6e246da90
Yun-Dong-Hee <dong000424@naver.com>
2023-03-10 14:33:07 +0900

    수정된파일입니다.

---
+++ language.js
@@ -0,0 +1,28 @@
+let lang = navigator.language;
+
+if(lang=="ko-KR") {
+    var change_lang = "언어 전환";
+    var title = "안녕하세요! 이 프로그램은 연습용 프로젝트입니다.";
+    var card_title_first = "첫번째";
+    var card_text_first = "여기는 학점을 계산을 하는 페이지입니다.";
+    var card_title_second = "두번째";
+    var card_text_second ="여기는 두번째페이지입니다.";
+    var card_title_third = "세번째";
+    var card_text_third ="여기는 세번째페이지입니다.";
+    var card_text_title ="공지사항"
+    var card_text_notice_content ="공지사항 입니다."
+    var card_text_notice_author ="윤동희가 제작했습니다."
+}
+if(lang="en-US") {
+    var change_lang = "Change language";
+    var title = "Hello! This program is a practice project";
+    var card_title_first = "First";
+    var card_text_first = "This is the page where you calculate your credits";
+    var card_title_second = "Second";
+    var card_text_second = "This is the second page";
+    var card_title_third = "Third";
+    var card_text_third = "This is the third page";
+    var card_text_title = "Notice"
+    var card_text_notice_content = "This is a notice."
+    var card_text_notice_author ="Created by Donghee Yun."
+}
